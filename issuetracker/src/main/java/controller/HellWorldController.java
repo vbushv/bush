53da682b6b34c5280a265f34bcdf062e23fd79b2
issuetracker <kk@kk-PC>
2016-10-03 22:22:58 +0900

    10/33

--- issuetracker/src/main/java/controller/HellWorldController.java
+++ issuetracker/src/main/java/controller/HellWorldController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import service.BlueService;
 import service.HelloService;
 
 @Controller
@@ -14,6 +15,9 @@ public class HellWorldController {
 	//HelloService service = new HelloServiceImpl(); //Servlet 방식
 	@Autowired // <-- 알아서 주입해줌 그냥 가갖다쓰면 됨
 	private HelloService service; // <-- 이거랑 위에꺼랑 똑같음
+	
+	@Autowired
+	private BlueService blueService;
 	
 	@RequestMapping("/hello/helloWorld") //url 주소를 만들어주는곳 RequestMapping 이건 스프링 문법중에 하나 url 만들어주는거 WebServlet 이랑 비슷한 개념
 	public String helloWorld(
@@ -50,7 +54,7 @@ public class HellWorldController {
 		
 		String msg = "안녕!! 공부 재미있지???";
 		
-		modelMap.addAttribute("msg", msg); // 쪼금 바ㅜ뀌었음 나머진 거의 동일하죠?
+		modelMap.addAttribute("msg", blueService.msg()); // 쪼금 바ㅜ뀌었음 나머진 거의 동일하죠?
 		
 		return "blue0546/dooga"; //jsp를 지정해줌 그러니까 jsp에 위 로직을 전부 보내줌 이것만 알면 되요
 		// 나머지는 컨테이너(스프링) 에서 알아서 다 해줌 
---
+++ issuetracker/src/main/java/dao/BlueDao.java
@@ -0,0 +1,13 @@
+package dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BlueDao {
+
+	public String msg(){
+		
+		return "메롱메롱메롱메롱";
+	}
+	
+}
